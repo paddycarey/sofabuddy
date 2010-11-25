42c033440bf3b51f7999c2ebf3472f2076b852ca
paddycarey <paddy@wackwack.co.uk>
2010-11-25 19:40:47 +0000

    Added GPLv3 License text and copyright notice

--- tvwrangler.py
+++ tvwrangler.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+#    tvwrangler.py - Auto sorter for tv episodes
+#    Copyright (C) 2010 Patrick Carey
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys, re, os, string, tvrage.api
 
 download_dir = '/media/somedir/downloads'
