761d9600ca723b6ac05cf6a3397f447a82bc96a0
paddycarey <paddy@wackwack.co.uk>
2010-11-30 01:05:57 +0000

    If not using default download_dir, print this also in error messages

--- tvwrangler.py
+++ tvwrangler.py
@@ -161,8 +161,14 @@ if __name__ == "__main__":
                     try:
                         moveshit = do_file_move(episode_info[0], episode_info[1], episode_info[2], episode_info[3], episode_info[4], episode_info[5], episode_info[6], episode_info[7])
                     except Exception as inst:
+                    if download_dir == config.get('directories', 'download_dir'):
                         print timestamp(), 'ERROR: MOVE:', "(", filename, ")", inst
+                    else:
+                        print timestamp(), 'ERROR: MOVE:', "(", download_dir, ")", "(", filename, ")", inst
                 except Exception as inst:
-                    print timestamp(), 'ERROR: IDENTIFY:', "(", filename, ")", inst
+                    if download_dir == config.get('directories', 'download_dir'):
+                        print timestamp(), 'ERROR: IDENTIFY:', "(", filename, ")", inst
+                    else:
+                        print timestamp(), 'ERROR: IDENTIFY:', "(", download_dir, ")", "(", filename, ")", inst
         lockup.close()
         os.remove(lockfile)
