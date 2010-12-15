14f5c20dc5500f054b5400481df99fe29fabdaa9
paddycarey <paddy@wackwack.co.uk>
2010-12-15 23:18:14 +0000

    Added explanatory comments to config file

--- example_config.py
+++ example_config.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 
+###############################################################################
 #   These are required variables, although they can also be set on the command
 #   line if you wish. sofabuddy.py will not run if these variables are not set.
+###############################################################################
 
+#   The location of your tv episodes to be sorted
 download_dir = '/media/some_dir/downloads'
+
+#   The location of your tv library
 tv_dir = '/media/some_dir/tv_library'
+
+#   The location that nuked files should be moved to
 nuke_dir = '/media/some_dir/nuked'
 
+###############################################################################
 #   These variables are optional. sofabuddy will use default values if they are
 #   not set.  You can uncomment each one to enable it as needed.
+###############################################################################
 
+#   The ip address or hostname of your xbmc box to send update commands to
 #xbmc_ip = '127.0.0.1'
+
+#   The location of the sofabuddy log file
 #log_file = '/tmp/sofabuddy_log'
+
+#   The location of the sofabuddy lock file
 #lock_file = '/tmp/sofabuddy_lock'
 
-#   Advanced config options
-#   Only change if you know what you are doing
+###############################################################################
+#   Advanced config options. Only change if you know what you are doing
+###############################################################################
 
+#   You can add additional regexes to this list to recognise more season and
+#   episode number formats
 #episode_number_regexes = [['s[0-9][0-9]e[0-9][0-9]', 1, 3, 4, 6], ['[0-9][0-9]x[0-9][0-9]', 0, 2, 3, 5], ['[0-9]x[0-9][0-9]', 0, 1, 2, 4], ['s[0-9][0-9] ep[0-9][0-9]', 1, 3, 6, 8]]
