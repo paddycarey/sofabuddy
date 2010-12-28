8654c4e555eef16596676cd4a42cbfe81b5da1b2
paddycarey <paddy@wackwack.co.uk>
2010-12-28 03:26:19 +0000

    slight logging change

--- libsofabuddy.py
+++ libsofabuddy.py
@@ -194,9 +194,7 @@ class file_operations:
         if not os.path.islink(self.episode_path_old):
             if os.path.isfile(self.episode_path_old):
                 shutil.move(self.episode_path_old, self.episode_path_new)
-                logMessage = 'MoveSrc=\"' + self.episode_path_old + '\"'
-                self.logger.info(logMessage)
-                logMessage = 'MoveDest=\"' + self.episode_path_new + '\"'
+                logMessage = 'Processed \"' + self.episode_path_old + '\" Show Name \"' + self.show_name + '\" Episode \"' + self.season_no + 'x' + self.episode_no + '\" Quality \"'
                 self.logger.info(logMessage)
                 os.symlink(self.episode_path_new, self.episode_path_old)
 
